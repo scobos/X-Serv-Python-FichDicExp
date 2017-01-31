2724ad85a45c549aebb700c50a3e6e4664684f21
Sergio Cobos Manjavacas <scobos@gamma16.aulas.gsyc.urjc.es>
2017-01-31 12:57:11 +0100

    Ejercicio 13.5

---
+++ fich_lists_dic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+fich= open("/etc/passwd","r")
+userShells= {}
+lineas= fich.readlines()
+for linea in lineas:
+	token = linea.split(':')
+	userShells[token[0]] = token[-1]
+	#print("User: " , token[0] , " Shell que utiliza: ", token [-1][:-1])
+fich.close()
+
+try:
+	print('root', userShells['root'])
+except KeyError:
+	print("Introduce un usuario existente")
